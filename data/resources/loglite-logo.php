7af934e54933907593fd9867f4bfe18e68cd8130
kaz6120 <kaz6120@gmail.com>
2018-12-11 10:54:45 +0900

    modified databases
    modified SQLite3 database.
    addeted LogLite logo generated by GD.

---
+++ data/resources/loglite-logo.php
@@ -0,0 +1,54 @@
+<?php
+// Create a new image
+$newCanvas = imagecreatetruecolor(200, 200);
+imageantialias($newCanvas, true);
+
+// text Settings
+$text_0 = 'LogLite';
+$text_1 = 'Super Simple Blog System';
+
+// Color settings
+$backgroundColor = imagecolorallocate($newCanvas, 41, 44, 52);
+// green = 128, 255, 0
+$arcColor = imagecolorallocate($newCanvas, 153, 255, 51);
+$textColour = imagecolorallocate($newCanvas, 153, 255, 51);
+$lineColor = imagecolorallocate($newCanvas, 153, 255, 51);
+
+
+//bool imagestring ( resource $image , int $font , int $x , int $y , string $string , int $color )
+// // 描画する文字列
+// $text = 'Testing...';
+// // フォント自身のパスでパスを置き換えます
+// $font = './ipaexg.ttf';
+// // テキストに影を付けます
+// imagettftext($newCanvas, 20, 0, 11, 21, $textColor, $font, $text);
+// // テキストを追加します
+// imagettftext($newCanvas, 5, 10, 10, 20, $textColor, $font, $text);
+
+
+imagestring($newCanvas, 5, 70, 75, $text_0, $textColour);
+imagestring($newCanvas, 2, 30, 105, $text_1, $textColour);
+
+//bool imagearc ( resource $image , int $cx , int $cy , int $width , int $height , int $start , int $end , int $color )
+// imagesetthickness ($newCanvas, 3);
+// imagearc($newCanvas, 100, 100, 190, 190,  0, 360, $arcColor);
+imagesetthickness ($newCanvas, 1);
+imagerectangle($newCanvas, 10, 10, 190, 190, $lineColor);
+
+// bool imageline ( resource $image , int $x1 , int $y1 , int $x2 , int $y2 , int $color )
+// Line 1
+imagesetthickness ($newCanvas, 1);
+imageline($newCanvas, 20, 60, 180, 60, $lineColor);
+// Line 2
+imagesetthickness ($newCanvas, 1);
+imageline($newCanvas, 20, 140, 180, 140, $lineColor);
+
+header("Content-type: image/png");
+imagepng($newCanvas);
+imagecolordeallocate($lineColor);
+imagecolordeallocate($textColor);
+imagecolordeallocate($backgroundColor);
+
+// Release memory
+imagedestroy($newCanvas);
+?>
--- plugins/showSamplesMenu.php
+++ plugins/showSamplesMenu.php
@@ -2,8 +2,8 @@
 /*
  これはshowSamplesMenuというプラグインです。navigationの部分のプラグインフィルターを使い、
  Expanderモジュールのサンプル例を紹介するリンクを表示します。
- 
-*/ 
+
+*/
 
 $this->plugin->addFilter('navigation', 'showSamplesMenu');
 
@@ -11,28 +11,26 @@ $this->plugin->addFilter('navigation', 'showSamplesMenu');
 function showSamplesMenu($text)
 {
     global $config, $pathToIndex;
-    
+
     switch ($config['language']) {
         case 'japanese':
             $textParts = array(
                 'マニュアル',
-                'このメニューを消すには、「/plugin/」フォルダにある「showSamplesMenu」プラグインを削除してください。'
+                'このメニューを消すには、「/plugin/」フォルダにある「showSamplesMenu」プラグインを削除するか「/plugin/off/」フォルダに移動してください。'
             );
             break;
         default:
             $textParts = array(
                 'Manual',
-                'Remove "/plugin/showSamplesMenu" plugin to disable this menu.'
+                'Delete or Remove "/plugins/showSamplesMenu.php" plugin to "/plugins/off" folder to disable this menu.'
             );
             break;
     }
- 
+
     return '<h2>References</h2>
 <ul class="menu">
-<li id="api-document">
-<a href="' . $pathToIndex . '/lib/Loggix/doc" title="Show Loggix API Document" class="menu">
-Loggix API Document
-</a>
+<li id="admin-login">
+<a href="' . $pathToIndex . '/admin/login.php">Login</a>
 </li>
 <li id="manual">
 <a href="' . $pathToIndex . '/modules/manual/" title="Show Loggix Manual" class="menu">
@@ -45,4 +43,3 @@ Loggix ' . $textParts[0] . '
 </div>
 ' . $text;
 }
-
